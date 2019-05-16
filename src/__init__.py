abcb6ec9ef925cac429d5c2d2be1fc046a606d3a
Hamim Al Mahdi Russell <russell310@gmail.com>
2019-05-17 02:48:49 +0600

    change folder structure

--- src/__init__.py
+++ src/__init__.py
@@ -26,7 +26,7 @@ def create_app(config_name):
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-    Migrate(app, db)
+    migrate = Migrate(app, db)
 
     # from app import models
 
